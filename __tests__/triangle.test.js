af524d80139d7066a6ffc9bea70a64b77b1fdf13
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-04-12 20:54:15 -0700

    pass test 2

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -1,4 +1,3 @@
-// import { TestScheduler } from "jest-cli";
 import Triangle from "./../src/triangle.js";
 
 describe("Triangle", () => {
@@ -8,5 +7,10 @@ describe("Triangle", () => {
     expect(triangle.side1).toEqual(2);
     expect(triangle.side2).toEqual(4);
     expect(triangle.side3).toEqual(5);
+  });
+
+  test("should correctly determine whether three lengths are not a triangle", () => {
+    const notTriangle = new Triangle(3,9,22);
+    expect(notTriangle.checkType()).toEqual("not a triangle");
   });
 });
